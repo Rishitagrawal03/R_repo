5111274c34a679fd42bd416ea6743c343364a43c
Rishitagrawal03 <81735543+Rishitagrawal03@users.noreply.github.com>
2023-02-16 11:58:56 +0530

    Add files via upload

---
+++ R/aiml.R
@@ -0,0 +1,19 @@
+#' create Histogram, boxplot, and numeric summary
+#' @export
+#' @param x numeric variable
+
+aiml <- function(x){
+  #1 row and 2 columns
+  par(mfrow  = c(1,2))
+  #Histogram
+  hist(x, col = rainbow(30))
+  #boxplot
+  boxplot(x, col = "green")
+  par(mfrow = c(1,1))
+  #numeric summary
+  data.frame(min =  min(x),
+             median = median(x),
+             max = max(x),
+             sd = sd(x),
+             range = max(x) - min(x))
+}
